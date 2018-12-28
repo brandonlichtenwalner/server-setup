7c147de9acd620c1247ec85a7acd1116df791cee
Brandon Lichtenwalner <brandon@brandonlichtenwalner.com>
2018-12-28 17:44:53 -0500

    add llblk alias

--- base/common/setup-common.sh
+++ base/common/setup-common.sh
@@ -2,7 +2,20 @@
 # setup-common.sh
 # setup configs & install packages common to bare metal and virtual installs
 
-# Check which type of system we're on and act appropriately
+
+
+### Items that apply to all (common) Linux distributions
+
+# Define personal aliases and add to /etc/bash.bashrc to add for all users
+alias llblk="lsblk --output NAME,SIZE,TYPE,FSTYPE,MOUNTPOINT,SERIAL,UUID"
+cat <<EOT >> /etc/bash.bashrc
+# Long listing of block devices
+alias llblk="lsblk --output NAME,SIZE,TYPE,FSTYPE,MOUNTPOINT,SERIAL,UUID"
+EOT
+
+
+
+### Check which type of system we're on and act appropriately
 case $(lsb_release -ds) in
   *"Ubuntu 18.04"*)
     # Proceed for Ubuntu 18.04
@@ -14,6 +27,6 @@ case $(lsb_release -ds) in
     ;;
   *)
     # Default case
-    echo "No actions defined for $(lsb_release -ds)"
+    echo "WARNING: No actions defined for $(lsb_release -ds). Processing default actions only."
     ;;
 esac
