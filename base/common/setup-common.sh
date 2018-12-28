384543a667051963141bfc5f90a2e8e4392eebfe
Brandon Lichtenwalner <brandon@brandonlichtenwalner.com>
2018-12-28 17:36:30 -0500

    switch from elif with grep to case

--- base/common/setup-common.sh
+++ base/common/setup-common.sh
@@ -3,10 +3,17 @@
 # setup configs & install packages common to bare metal and virtual installs
 
 # Check which type of system we're on and act appropriately
-if lsb_release -ds | grep -o "Ubuntu 18.04" >/dev/null 2>&1; then
-  # Proceed for Ubuntu 18.04
-elif lsb_release -ds | grep -o "CentOS Linux release 7" >/dev/null 2>&1; then
-  # Proceed for CentOS 7
-else
-  echo "No actions defined for $(lsb_release -ds)"
-fi
+case $(lsb_release -ds) in
+  *"Ubuntu 18.04"*)
+    # Proceed for Ubuntu 18.04
+    echo "Ubuntu 18.04 test"
+    ;;
+  *"CentOS Linux release 7"*)
+    # Proceed for CentOS 7
+    echo "CentOS 7 test"
+    ;;
+  *)
+    # Default case
+    echo "No actions defined for $(lsb_release -ds)"
+    ;;
+esac
