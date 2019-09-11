928129d6ff341f775f54d413b7056ec4077ad01e
Brandon Lichtenwalner <brandon@brandonlichtenwalner.com>
2019-09-11 05:25:24 -0400

    fix package names and add notes

--- setup-guacamole.sh
+++ setup-guacamole.sh
@@ -9,8 +9,7 @@ TOMCATVERSION=9
 
 
 # Get the files and check integrity
-
-wget http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/$GUACVERSION/binary/guacamole-$GUACVERSION.war
+wget https://mirror.reverse.net/pub/apache/guacamole/$GUACVERSION/binary/guacamole-$GUACVERSION.war
 wget https://www.apache.org/dist/guacamole/$GUACVERSION/binary/guacamole-$GUACVERSION.war.sha256
 
 sha256sum -c guacamole-$GUACVERSION.war.sha256
@@ -19,22 +18,24 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-wget http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/$GUACVERSION/source/guacamole-server-$GUACVERSION.tar.gz
-https://www.apache.org/dist/guacamole/1.0.0/source/guacamole-client-$GUACVERSION.tar.gz.sha256
+wget https://mirror.reverse.net/pub/apache/guacamole/$GUACVERSION/source/guacamole-server-$GUACVERSION.tar.gz
+wget https://www.apache.org/dist/guacamole/1.0.0/source/guacamole-server-$GUACVERSION.tar.gz.sha256
 
-sha256sum -c guacamole-$GUACVERSION.tar.gz.sha256
+sha256sum -c guacamole-server-$GUACVERSION.tar.gz.sha256
 if [ $? != 0 ]; then
-  echo "ERROR: guacamole-$GUACVERSION.tar.gz checksum is not valid!"
+  echo "ERROR: guacamole-server-$GUACVERSION.tar.gz checksum is not valid!"
   exit 1
 fi
 
 
 
 # Update packages and install build environment, required packages, optional packages, and tomcat
+# NOTE: libjpeg62-dev not installed due to conflict with libjpeg-turbo8-dev
+# NOTE: optional screen recording packages not installed
 sudo apt -y update
 sudo apt -y install gcc-6 g++-6 \
-libcairo2-dev libjpeg-turbo8-dev libjpeg62-dev libpng12-dev libossp-uuid-dev \
-libfreerdp-dev libpango-1.0-dev libssh2-1-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev \
+libcairo2-dev libjpeg-turbo8-dev libpng-dev libossp-uuid-dev \
+libfreerdp-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev \
 tomcat$TOMCATVERSION tomcat$TOMCATVERSION-admin tomcat$TOMCATVERSION-user
 
 if [ $? != 0 ]; then
