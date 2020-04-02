6343a58bc30caae55dbf2255193ee80e55edf807
Brandon Lichtenwalner <brandon@brandonlichtenwalner.com>
2020-04-02 14:28:17 -0400

    add setup-syncthing.sh

---
+++ setup-syncthing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# setup-syncthing.sh
+
+
+# Make sure the script has been executed as root
+if [ "$USER" != 'root' ]; then
+        echo "This script must be run with root privileges."
+        echo "Try using: sudo $0 $@"
+        exit 2
+fi
+
+# Install Syncthing snap
+snap install syncthing
+
+# Download Syncthing service file for servers to default systemd folder
+wget https://raw.githubusercontent.com/syncthing/syncthing/master/etc/linux-systemd/system/syncthing%40.service -P /etc/systemd/system
+
+# Enable & start the Syncthing service
+systemctl enable --now syncthing.service
+
+echo "Finished executing $0"
